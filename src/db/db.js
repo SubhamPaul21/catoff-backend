44108c850f2dd7e2861dc2936f122ad702783649
akshit1070 <akshit.garg47@gmail.com>
2024-02-20 19:20:37 +0530

    frame

---
+++ src/db/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI)
+
+
+
+
+const testDbConnection = async () => {
+    try {
+      await sequelize.authenticate();
+      await sequelize.sync({ alter: true });
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  };
+  testDbConnection()
+
+module.exports = { sequelize: sequelize, testDbConnection };
+
+
