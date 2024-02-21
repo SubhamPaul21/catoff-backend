3b9c4104ed5a44fd6b28a46d20f79229c26f5b2b
akshit1070 <akshit.garg47@gmail.com>
2024-02-21 15:09:39 +0530

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+const { sequelize } = require("../db/db.js");
+
+
+
+
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define('User', {
+    UserID: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    Email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true
+    },
+    UserName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true
+    },
+    RegistrationDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    LastLoginDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    IsEmailVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    IsActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    // Password: Handle encryption in your application logic
+  }, {
+    tableName: 'Users',
+    timestamps: false
+  });
+  
+  User.sync().then(() => {
+    console.log("User Model synced");
+  });
+  module.exports = User;
---
+++ src/models/walletAddress.model.js
@@ -0,0 +1,57 @@
+
+const { sequelize } = require("../db/db.js");
+
+
+
+
+const { DataTypes } = require("sequelize");
+var User = require("./user.model.js")
+
+
+
+
+const WalletAddress = sequelize.define('WalletAddress', {
+    WalletID: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    UserID: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'UserID'
+      }
+    },
+    WalletAddress: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true
+    },
+    IsVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    Signature: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    AddedDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+  }, {
+    tableName: 'WalletAddresses',
+    timestamps: false
+  });
+  
+
+  WalletAddress.sync().then(() => {
+    console.log("WalletAddress Model synced");
+  });
+  
+  
+  module.exports = WalletAddress;
