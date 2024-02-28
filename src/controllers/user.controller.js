5e5b61d571658a30253e3d47ff97b84c8e4ef2b8
ThomasJose1999 <thomas.jose@phaethon.tech>
2024-02-28 12:47:58 +0530

    auth tested with signature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,20 +4,18 @@ let {makeResponse} = require('../utils/responseMaker')
 module.exports.addUserDetails = async (req, res, next) => {
     try {
         const {email, username } = req.body;
-        console.log(req.userId, email, username);
         let newUser = await AddUserDetails(req.userId, email, username);
         return makeResponse(res, 200, true, "user registration successful", newUser)
     } catch (e) {
         console.log(e)
-        return makeResponse(res, 500, false, "user registration failed")
+        return makeResponse(res, 500, false, "user registration failed");
     }
 }
 
 module.exports.login = async (req, res, next) => {
     try {
         const {signature, message, publicKey } = req.body;
-        // console.log(signature, message, publicKey);
-        let token = await siwsVerification(signature, message, publicKey)
+        let token = await siwsVerification(signature, message, publicKey);
         return makeResponse(res, 200, true, "login successful", {token});
     } catch (e) {
         console.log(e)
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,10 @@
 let User =  require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { Connection } = require('@solana/web3.js');
-let ExpressError = require('../utils/expressErrors')
+const { Connection, PublicKey } = require('@solana/web3.js');
+let ExpressError = require('../utils/expressErrors');
+const nacl = require('tweetnacl');
+const bs58 = require('bs58');
 require('dotenv').config();
 
 
@@ -50,8 +52,14 @@ module.exports.AddUserDetails = async (userId, email, userName) => {
 module.exports.siwsVerification = async (signature, message, publicKey)=>{
     try{
         let user;
-        const solanaConnection = new Connection(process.env.RPC_URL);
-        const verified = await solanaConnection.verifyMessage(new TextEncoder().encode(message), signature, publicKey);
+        const decodedPublicKey = bs58.decode(publicKey);
+        const publicKeyUint8Array = new Uint8Array(decodedPublicKey);
+        const signatureBuffer = Buffer.from(bs58.decode(signature));
+        const verified = nacl.sign.detached.verify(
+            new TextEncoder().encode(message),
+            signatureBuffer,
+            publicKeyUint8Array
+        );
         if(!verified){
             throw new ExpressError("Invalid Signature",401);
         }
@@ -69,7 +77,7 @@ module.exports.siwsVerification = async (signature, message, publicKey)=>{
             });
             user.save()
         }
-        const token = jwt.sign({ userId: user.UserId }, process.env.JWT_SECRET, {
+        const token = jwt.sign({ userId: user.UserID }, process.env.JWT_SECRET, {
             expiresIn: '1h',
         });
         await user.update({ LastLoginDate: new Date() });
