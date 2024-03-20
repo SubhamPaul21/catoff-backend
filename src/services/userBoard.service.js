67c1ad6665e95feac899e5a0114dff6096695b18
Aryamanraj <20cs3011@rgipt.ac.in>
2024-03-20 23:19:10 +0530

    userBoard api corrections

--- src/services/userBoard.service.js
+++ src/services/userBoard.service.js
@@ -3,6 +3,7 @@ const {
   Challenge,
   Transaction,
   User,
+  Game,
   // WalletAddress,
 } = require('../models/index');
 const { Op } = require('sequelize');
@@ -10,7 +11,7 @@ const logger = require('../utils/logger'); // Ensure this path is correct for yo
 
 // const getUserCurrentStandings = async (userId) => {
 //   logger.debug(
-//     `[UserService] Fetching current standings for user ID: ${userId}`
+//     `[UserBoardService] Fetching current standings for user ID: ${userId}`
 //   );
 //   try {
 //     // Step 1: Fetch all player records for the given userId
@@ -75,102 +76,178 @@ const logger = require('../utils/logger'); // Ensure this path is correct for yo
 //     );
 
 //     logger.info(
-//       `[UserService] Current standings fetched successfully for user ID: ${userId}`
+//       `[UserBoardService] Current standings fetched successfully for user ID: ${userId}`
 //     );
 //     return formattedResults;
 //   } catch (error) {
 //     logger.error(
-//       `[UserService] Error fetching user standings for user ID: ${userId}: ${error}`
+//       `[UserBoardService] Error fetching user standings for user ID: ${userId}: ${error}`
+//     );
+//     throw error;
+//   }
+// };
+
+// const getUserCurrentStandings = async (userId) => {
+//   logger.debug(
+//     `[UserBoardService] Fetching current standings for user ID: ${userId}`
+//   );
+//   try {
+//     const userChallenges = await Player.findAll({
+//       where: { UserID: userId },
+//       include: [
+//         {
+//           model: Challenge,
+//           attributes: [
+//             'ChallengeID',
+//             'ChallengeName',
+//             'ChallengeType',
+//             'StartDate',
+//             'EndDate',
+//             'IsActive',
+//             'Wager',
+//             'Winners',
+//           ],
+//         },
+//       ],
+//       attributes: ['ChallengeID'],
+//     });
+
+//     const results = await Promise.all(
+//       userChallenges.map(async ({ Challenge }) => {
+//         const now = new Date();
+//         const challengeStatus =
+//           Challenge.IsActive &&
+//           now >= Challenge.StartDate &&
+//           now <= Challenge.EndDate
+//             ? 'Active'
+//             : now > Challenge.EndDate
+//               ? 'Completed'
+//               : 'Upcoming';
+
+//         const allParticipants = await Player.findAll({
+//           where: { ChallengeID: Challenge.ChallengeID },
+//           order: [['Value', 'DESC']],
+//           attributes: ['UserID', 'Value'],
+//         });
+
+//         // Calculate rank
+//         const rank =
+//           allParticipants.findIndex(
+//             (participant) => participant.UserID === userId
+//           ) + 1;
+//         const currentUserParticipation = allParticipants.find(
+//           (participant) => participant.UserID === userId
+//         );
+//         // Ensure Winners is an array and contains the userId
+//         const isWinner =
+//           Array.isArray(Challenge.Winners) &&
+//           Challenge.Winners.includes(userId);
+//         const adjustedWager = isWinner ? Challenge.Wager : -Challenge.Wager;
+
+//         return {
+//           challengeId: Challenge.ChallengeID,
+//           challengeName: Challenge.ChallengeName,
+//           challengeType: Challenge.ChallengeType,
+//           challengeStatus,
+//           startDate: Challenge.StartDate,
+//           endDate: Challenge.EndDate,
+//           wagerAmount:
+//             challengeStatus === 'Completed' ? adjustedWager : Challenge.Wager,
+//           currentValue: currentUserParticipation
+//             ? currentUserParticipation.Value
+//             : 0, // Assumes Value is stored in Player model
+//           rank,
+//         };
+//       })
+//     );
+
+//     logger.info(
+//       `[UserBoardService] Current standings fetched successfully for user ID: ${userId}`
+//     );
+//     return results;
+//   } catch (error) {
+//     logger.error(
+//       `[UserBoardService] Error fetching user standings for user ID: ${userId}: ${error}`
 //     );
 //     throw error;
 //   }
 // };
 
 const getUserCurrentStandings = async (userId) => {
-  logger.debug(
-    `[UserService] Fetching current standings for user ID: ${userId}`
-  );
+  logger.debug(`[UserService] Fetching current standings for user ID: ${userId}`);
   try {
     const userChallenges = await Player.findAll({
       where: { UserID: userId },
       include: [
         {
           model: Challenge,
+          as: 'challenge', // Ensure the alias matches the association definition
           attributes: [
             'ChallengeID',
             'ChallengeName',
-            'ChallengeType',
             'StartDate',
             'EndDate',
             'IsActive',
             'Wager',
-            'Winners',
+            'Winner',
+            'MaxParticipants',
+            'Players',
+            'Media',
+            'Target',
           ],
+          include: [{
+            model: Game,
+            as: 'game', // Ensure the alias matches the association definition
+            attributes: ['GameName', 'ParticipationType', 'GameType', 'GameDescription'],
+          }],
         },
       ],
       attributes: ['ChallengeID'],
     });
 
-    const results = await Promise.all(
-      userChallenges.map(async ({ Challenge }) => {
-        const now = new Date();
-        const challengeStatus =
-          Challenge.IsActive &&
-          now >= Challenge.StartDate &&
-          now <= Challenge.EndDate
-            ? 'Active'
-            : now > Challenge.EndDate
-              ? 'Completed'
-              : 'Upcoming';
+    const results = await Promise.all(userChallenges.map(async ({ challenge }) => {
+      const now = new Date();
+      const challengeStatus = challenge.IsActive && now >= challenge.StartDate && now <= challenge.EndDate
+        ? 'Active'
+        : now > challenge.EndDate
+            ? 'Completed'
+            : 'Upcoming';
 
-        const allParticipants = await Player.findAll({
-          where: { ChallengeID: Challenge.ChallengeID },
-          order: [['Value', 'DESC']],
-          attributes: ['UserID', 'Value'],
-        });
+      const allParticipants = await Player.findAll({
+        where: { ChallengeID: challenge.ChallengeID },
+        order: [['Value', 'DESC']],
+        attributes: ['UserID', 'Value'],
+      });
 
-        // Calculate rank
-        const rank =
-          allParticipants.findIndex(
-            (participant) => participant.UserID === userId
-          ) + 1;
-        const currentUserParticipation = allParticipants.find(
-          (participant) => participant.UserID === userId
-        );
-        // Ensure Winners is an array and contains the userId
-        const isWinner =
-          Array.isArray(Challenge.Winners) &&
-          Challenge.Winners.includes(userId);
-        const adjustedWager = isWinner ? Challenge.Wager : -Challenge.Wager;
+      const rank = allParticipants.findIndex(participant => participant.UserID === userId) + 1;
+      const totalParticipants = allParticipants.length; // Total number of participants in the challenge
+      const totalWager = challenge.Wager * totalParticipants;
 
-        return {
-          challengeId: Challenge.ChallengeID,
-          challengeName: Challenge.ChallengeName,
-          challengeType: Challenge.ChallengeType,
-          challengeStatus,
-          startDate: Challenge.StartDate,
-          endDate: Challenge.EndDate,
-          wagerAmount:
-            challengeStatus === 'Completed' ? adjustedWager : Challenge.Wager,
-          currentValue: currentUserParticipation
-            ? currentUserParticipation.Value
-            : 0, // Assumes Value is stored in Player model
-          rank,
-        };
-      })
-    );
+      return {
+        challengeId: challenge.ChallengeID,
+        challengeName: challenge.ChallengeName,
+        gameName: challenge.game.GameName, // Newly added Game details
+        participationType: challenge.game.ParticipationType,
+        gameType: challenge.game.GameType,
+        gameDescription: challenge.game.GameDescription,
+        challengeStatus,
+        startDate: challenge.StartDate,
+        endDate: challenge.EndDate,
+        wagerAmount: totalWager, // Reflects total wager for consistency
+        currentValue: totalWager, // Assuming it should reflect the total wager involved
+        rank,
+      };
+    }));
 
-    logger.info(
-      `[UserService] Current standings fetched successfully for user ID: ${userId}`
-    );
+    logger.info(`[UserService] Current standings fetched successfully for user ID: ${userId}`);
     return results;
   } catch (error) {
-    logger.error(
-      `[UserService] Error fetching user standings for user ID: ${userId}: ${error}`
-    );
+    logger.error(`[UserService] Error fetching user standings for user ID: ${userId}: ${error}`);
     throw error;
   }
 };
+
+
 
 // Helper function to format date as YYYY-MM-DD HH:MM where MM is the nearest 15-minute block
 const formatToNearest15Min = (date) => {
@@ -181,86 +258,84 @@ const formatToNearest15Min = (date) => {
 };
 
 const getUserProgressData = async (userId, period) => {
-  logger.debug(
-    `[UserService] Fetching user progress data for ID: ${userId}, Period: ${period}`
-  );
+  logger.debug(`[UserBoardService] Fetching user progress data for ID: ${userId}, Period: ${period}`);
   let dateFrom;
-  let aggregateByInterval = false; // Adjusted for clarity
+  let aggregateByInterval = false;
 
   switch (period) {
     case '30days':
       dateFrom = new Date(new Date().setDate(new Date().getDate() - 30));
       break;
     case '24hours':
-      // Ensure dateFrom is exactly 24 hours before now
       dateFrom = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
-      aggregateByInterval = true;
+      aggregateByInterval = true; // Aggregate by the nearest 15-minute interval
       break;
     case 'all':
       dateFrom = new Date(0); // Start from the Unix Epoch (1970-01-01)
       break;
     default:
-      logger.error(
-        '[UserService] Invalid period specified for user progress data'
-      );
+      logger.error('[UserBoardService] Invalid period specified for user progress data');
       throw new Error('Invalid period specified');
   }
 
   try {
+    // Fetch transactions where the user is either the sender or the receiver
     const transactions = await Transaction.findAll({
       where: {
-        UserID: userId,
-        Timestamp: {
-          [Op.gte]: dateFrom,
-        },
+        [Op.or]: [{ To: userId }, { From: userId }],
+        Timestamp: { [Op.gte]: dateFrom },
       },
       order: [['Timestamp', 'ASC']],
-      attributes: ['Amount', 'Timestamp'],
+      attributes: ['Amount', 'Timestamp', 'To', 'From'],
     });
 
-    // Aggregate data for graph
-    const aggregatedData = transactions.reduce((acc, { Amount, Timestamp }) => {
-      const key = aggregateByInterval
-        ? formatToNearest15Min(Timestamp)
-        : Timestamp.toISOString().split('T')[0];
-      if (!acc[key]) acc[key] = 0;
-      acc[key] += Amount;
+    // Aggregate data for graph, considering credits and debits
+    const aggregatedData = transactions.reduce((acc, { Amount, Timestamp, To, From }) => {
+      const key = aggregateByInterval ? formatToNearest15Min(Timestamp) : Timestamp.toISOString().split('T')[0];
+      if (!acc[key]) acc[key] = { credits: 0, debits: 0 };
+
+      // If the user is the receiver, it's a credit; if the sender, it's a debit
+      if (To === userId) {
+        acc[key].credits += Amount;
+      } else if (From === userId) {
+        acc[key].debits -= Amount; // Assuming you want to show debits as negative
+      }
+
       return acc;
     }, {});
 
-    const creditsGraph = Object.entries(aggregatedData).map(
-      ([time, creditsEarned]) => ({ time, creditsEarned })
-    );
+    const creditsGraph = Object.entries(aggregatedData).map(([time, { credits, debits }]) => ({
+      time,
+      balanceChange: credits + debits, // Combine credits and debits for the net balance change
+    }));
 
-    logger.info(
-      `[UserService] User progress data fetched successfully for ID: ${userId}`
-    );
+    logger.info(`[UserBoardService] User progress data fetched successfully for ID: ${userId}`);
     return { creditsGraph };
   } catch (error) {
-    logger.error(
-      `[UserService] Error fetching user progress data for ID: ${userId}: ${error}`
-    );
+    logger.error(`[UserBoardService] Error fetching user progress data for ID: ${userId}: ${error}`);
     throw error;
   }
 };
 
+
 const getUserDetailsData = async (userId) => {
-  logger.debug(`[UserService] Fetching user details data for ID: ${userId}`);
+  logger.debug(`[UserBoardService] Fetching user details data for ID: ${userId}`);
   try {
-    // Fetch user basic details
+    // Fetch user basic details including WalletAddress directly
     const user = await User.findOne({
       where: { UserID: userId },
-      include: [
-        {
-          model: WalletAddress,
-          attributes: ['WalletAddress'], // Assuming WalletAddress model has a WalletAddress field
-        },
+      attributes: [
+        'UserID',
+        'Email',
+        'UserName',
+        'WalletAddress',
+        'Credits',
+        'ProfilePicture',
       ],
-      attributes: ['UserID', 'Email', 'UserName', 'WalletID', 'Credits'],
     });
 
     if (!user) {
-      logger.info(`[UserService] User not found for ID: ${userId}`);
+      logger.info(`[UserBoardService] User not found for ID: ${userId}`);
       throw new Error('User not found');
     }
 
@@ -272,7 +347,6 @@ const getUserDetailsData = async (userId) => {
       })
     ).map((participation) => participation.ChallengeID);
 
-    // Define variables for past and active challenges count initialization
     let pastChallengesCount = 0;
     let currentActiveChallengesCount = 0;
     let currentStaked = 0;
@@ -307,22 +381,23 @@ const getUserDetailsData = async (userId) => {
       (await Transaction.sum('Amount', { where: { UserID: userId } })) || 0;
 
     logger.info(
-      `[UserService] User details data fetched successfully for ID: ${userId}`
+      `[UserBoardService] User details data fetched successfully for ID: ${userId}`
     );
     return {
       UserID: userId,
       UserName: user.UserName,
       UserEmail: user.Email,
-      WalletAddress: user.WalletAddress?.WalletAddress || 'N/A', // Handle case where WalletAddress might be null
-      TotalRewardsWon: totalRewardsWon,
+      WalletAddress: user.WalletAddress || 'N/A', // Now directly accessed
       Credits: user.Credits,
+      ProfilePicture: user.ProfilePicture, // Now directly accessed
       PastChallenges: pastChallengesCount,
       CurrentActiveChallenges: currentActiveChallengesCount,
       CurrentStaked: currentStaked,
+      TotalRewardsWon: totalRewardsWon,
     };
   } catch (error) {
     logger.error(
-      `[UserService] Error fetching user details data for ID: ${userId}: ${error}`
+      `[UserBoardService] Error fetching user details data for ID: ${userId}: ${error}`
     );
     throw error;
   }
